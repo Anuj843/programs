f0e491d98aa3634cd129f5a470da8150725d2536
Anuj <anujsharma.as957@gmail.com>
2023-02-28 01:52:38 +0530

    Added string functing

--- Chapter 03/03_string_function.py
+++ Chapter 03/03_string_function.py
@@ -1,2 +1,11 @@
-story = "Once upon a stime there's a boy name Anuj, who likes exploring new places"
+story="Once upon a stime there's a boy name Anuj, who likes exploring new places"
 print(story)
+print(len(story))
+print(story.endswith("k"))
+print(story.count("Anuj"))
+print(story.capitalize())
+print(story.find("boy"))
+print(story.replace("Anuj","Sharmaji"))
+
+
+
