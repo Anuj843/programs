371fb3b2e9f015b2b411fc5409f1a1a421f1b22e
Anuj <anujsharma.as957@gmail.com>
2023-03-01 01:34:31 +0530

    started list chapter

---
+++ Chapter_04/01_lists.py
@@ -0,0 +1,13 @@
+# Create a list using []
+a = [3,7,5,8,4,10]
+print(a)
+# Access using index a[0], a[4],a[-2]
+print(a[5])
+# Change the value of the list using variable
+a[2] = 50
+print(a)
+
+# We can create a list using different types
+b = [12, "Anuj", False, True, None, 67.5]
+print(b)
+print(b[1])
