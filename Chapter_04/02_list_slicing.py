94f261d796fb8a12488ccd0978adb773785d3640
Anuj <anujsharma.as957@gmail.com>
2023-03-01 02:30:18 +0530

    Added list slicing and method

---
+++ Chapter_04/02_list_slicing.py
@@ -0,0 +1,6 @@
+# List slicing
+friends = ["Anuj", "Rehman", "Tom", "Harry"]
+print(friends[0:2])
+print(friends[0:])
+print(friends[-4:-2])
+print(friends[-1:])
---
+++ Chapter_04/03_list_methods.py
@@ -0,0 +1,9 @@
+l1 = [1, 8, 7, 2, 21, 15]
+# list methods doesn't require a variable
+# l1.sort() # Sort the list in accending order
+# l1.reverse() # Reverses the list
+# l1.append(45) # Append means adding, here 45 will add at the end of list.(VERY IMP METHOD)
+# l1.insert(2,544) # Inserts 544 at index 2
+# l1.pop(2) # Remove element at index 2 from the list
+l1.remove(21) # Remove 21 from the list
+print(l1)
