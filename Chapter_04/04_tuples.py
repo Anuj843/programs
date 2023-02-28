68dccc4814bf37b39fc477e08bbc649cd26a602a
Anuj <anujsharma.as957@gmail.com>
2023-03-01 02:58:00 +0530

    Added tuple slicing and methods

---
+++ Chapter_04/04_tuples.py
@@ -0,0 +1,10 @@
+# Creating a Tuple using ()
+t=(1, 2, 2, 5, 8)
+# Printing the elements of tuple
+print(t[0])
+# Cannot update the value of a tuple
+# For eg: t[0]=15(Throws error)
+# tuple is a immutable data type in python
+# t1=() # Empty tuple
+# t1=(1) # Wrong way to declare a tuple with single element
+# t1=(1,) # Tuple with single element
---
+++ Chapter_04/05_tuple methods.py
@@ -0,0 +1,5 @@
+# Creating a Tuple using ()
+t=(1, 2, 4, 5, 1, 2, 1, 1)
+# Methods of tuple
+print(t.count(1)) # Counts the elemts for number of times
+print(t.index(5)) # Shows the index of an element
